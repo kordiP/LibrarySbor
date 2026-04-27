ab5fcf2dac10e3d96b1899694f84efc86b175827
Atanas Kordov <nasikordov@gmail.com>
2026-04-27 14:36:49 +0300

    add basic pages

--- LibrarySbor/src/app/app-routing.module.ts
+++ LibrarySbor/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HomePageComponent } from './screens/home-page/home-page.component';
+import { HistoryPageComponent } from './screens/history-page/history-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+
+  {
+    path: 'history',
+    component: HistoryPageComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- LibrarySbor/src/app/app.module.ts
+++ LibrarySbor/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './screens/home-page/home-page.component';
+import { HistoryPageComponent } from './screens/history-page/history-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HistoryPageComponent
   ],
   imports: [
     BrowserModule,
