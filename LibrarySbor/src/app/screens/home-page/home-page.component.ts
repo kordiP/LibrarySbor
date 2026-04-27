8bdea0514cfd994e55a3c2530a118f9f0cc64192
Atanas Kordov <nasikordov@gmail.com>
2026-04-27 15:37:38 +0300

    Added ngb correctly + some basic info

--- LibrarySbor/src/app/screens/home-page/home-page.component.ts
+++ LibrarySbor/src/app/screens/home-page/home-page.component.ts
@@ -6,12 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit {
-
   constructor() {
-
   }
 
+  images: string[] = [];
+
   ngOnInit(): void {
-    
+    this.images.push("assets/images/CHANGE.jpg");
   }
 }
